e1d98016d755920d0917d85dd6eae84ffa0823c5
Huangzq <huangzq@zhengtongauto.net>
2019-03-11 16:08:58 +0800

    springcloud initial

---
+++ config/src/test/java/com/hzq/demo/ConfigAppTest.java
@@ -0,0 +1,20 @@
+package com.hzq.demo;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class ConfigAppTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
---
+++ register/src/test/java/com/hzq/demo/RegisterAppTest.java
@@ -0,0 +1,20 @@
+package com.hzq.demo;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class RegisterAppTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
