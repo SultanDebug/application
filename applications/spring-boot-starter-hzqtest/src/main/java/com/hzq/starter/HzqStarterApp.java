d412996953de1bd5d5d8ce652f733cc5465a76e3
Huangzq <huangzq@zhengtongauto.net>
2019-09-12 14:31:15 +0800

    starter测试

---
+++ applications/spring-boot-starter-hzqtest/src/main/java/com/hzq/starter/HzqStarterApp.java
@@ -0,0 +1,13 @@
+package com.hzq.starter;
+
+/**
+ * Hello world!
+ *
+ */
+public class HzqStarterApp
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ applications/spring-boot-starter-hzqtest/src/test/java/com/hzq/starter/HzqStarterAppTest.java
@@ -0,0 +1,20 @@
+package com.hzq.starter;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple HzqStarterApp.
+ */
+public class HzqStarterAppTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
