b9683d1c50cd79916fb594399aec26ffea488aab
Huangzq <huangzq@zhengtongauto.net>
2019-09-16 09:26:27 +0800

    spring-cloud-gateway测试

---
+++ applications/cloud-gateway/src/main/java/com/hzq/cloudgateway/SpringCloudGatewayApp.java
@@ -0,0 +1,17 @@
+package com.hzq.cloudgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+public class SpringCloudGatewayApp
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(SpringCloudGatewayApp.class,args);
+    }
+}
---
+++ applications/cloud-gateway/src/test/java/com/hzq/cloudgateway/SpringCloudGatewayAppTest.java
@@ -0,0 +1,20 @@
+package com.hzq.cloudgateway;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple SpringCloudGatewayApp.
+ */
+public class SpringCloudGatewayAppTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
