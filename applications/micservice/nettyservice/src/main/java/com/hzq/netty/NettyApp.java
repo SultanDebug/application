e39aa35739a1e0d2ece6e231a0f2983e21f6b72f
Sultan <hzqsultan@qq.com>
2020-01-05 22:52:53 +0800

    bio nio netty学习

---
+++ applications/micservice/nettyservice/src/main/java/com/hzq/netty/NettyApp.java
@@ -0,0 +1,18 @@
+package com.hzq.netty;
+
+import com.hzq.common.annotation.EnableApplication;
+import org.springframework.boot.SpringApplication;
+
+/**
+ * Hello world!
+ *
+ */
+@EnableApplication
+public class NettyApp
+{
+    public static void main( String[] args )
+    {
+
+        SpringApplication.run(NettyApp.class, args);
+    }
+}
