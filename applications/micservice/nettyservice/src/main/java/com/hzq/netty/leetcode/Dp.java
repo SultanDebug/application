09fe732ff8af6240eedd56b3d6b36075840481f9
Huangzq <huangzq@zhengtongauto.net>
2020-04-24 17:28:44 +0800

    leetcode

--- applications/micservice/nettyservice/src/main/java/com/hzq/netty/leetcode/Dp.java
+++ applications/micservice/nettyservice/src/main/java/com/hzq/netty/leetcode/Dp.java
@@ -1,5 +1,11 @@
 package com.hzq.netty.leetcode;
 
+import com.google.common.primitives.UnsignedInteger;
+import org.apache.commons.lang3.ArrayUtils;
+
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @Description: TODO
  * @Auth: Huangzq
@@ -9,40 +15,297 @@ public class Dp {
     public static void main(String[] args) {
         int a = 3;
 
+        int[] b = {7, 5, 1, 6, 4};
+
         Dp dp = new Dp();
-        System.out.println(dp.divisorGame(a));
+        int[] res = dp.mergeSort(b);
+        System.out.println(res);
+//        System.out.println(dp.hammingWeight(5));
+//        System.out.println(dp.waysToChange(900750));
     }
 
     /**
+     * mst51. 数组中的逆序对
+     * 在数组中的两个数字，如果前面一个数字大于后面的数字，则这两个数字组成一个逆序对。输入一个数组，求出这个数组中的逆序对的总数
+     *
+     * @param nums
+     * @return
+     */
+    public int reversePairs(int[] nums) {
+
+
+        return 0;
+    }
+
+    /**
+     * 归并计数逆序对
+     *
+     * @param nums
+     * @return
+     */
+    public int mergeCount(int[] nums) {
+
+        int avg = nums.length / 2;
+
+        int left, right, res;
+
+        if (nums.length <= 1) {
+            return 0;
+        } else {
+            int[] tmp1 = new int[avg];
+            System.arraycopy(nums, 0, tmp1, 0, avg);
+
+            int[] tmp2 = new int[nums.length - avg];
+            System.arraycopy(nums, avg, tmp2, 0, nums.length - avg);
+
+            left = mergeCount(tmp1);
+            right = mergeCount(tmp2);
+        }
+
+//        int[] res = new int[nums.length];
+
+        int k = 0;
+        int i = 0, j = 0;
+
+        boolean lf = false, rf = false;
+
+        /*while (k < nums.length
+                && i < left.length
+                && j < right.length) {
+            if (left[i] > right[j]) {
+
+                if (rf) {
+                    res[k] = left[i];
+                    i++;
+                } else {
+                    res[k] = right[j];
+                    if (j == right.length - 1) {
+                        rf = true;
+                    } else {
+                        j++;
+                    }
+                }
+
+
+            } else {
+                if (lf) {
+                    res[k] = right[j];
+                    j++;
+                } else {
+                    res[k] = left[i];
+                    if (i == left.length - 1) {
+                        lf = true;
+                    } else {
+                        i++;
+                    }
+                }
+            }
+            k++;
+        }*/
+
+        return 0;
+    }
+
+    /**
+     * 归并排序
+     *
+     * @param nums
+     * @return
+     */
+    public int[] mergeSort(int[] nums) {
+
+        int avg = nums.length / 2;
+
+        int[] left, right;
+
+        if (nums.length <= 1) {
+            return nums;
+        } else {
+            int[] tmp1 = new int[avg];
+            System.arraycopy(nums, 0, tmp1, 0, avg);
+
+            int[] tmp2 = new int[nums.length - avg];
+            System.arraycopy(nums, avg, tmp2, 0, nums.length - avg);
+
+            left = mergeSort(tmp1);
+            right = mergeSort(tmp2);
+        }
+
+        int[] res = new int[nums.length];
+
+        int k = 0;
+        int i = 0, j = 0;
+
+        boolean lf = false, rf = false;
+
+        while (k < nums.length
+                && i < left.length
+                && j < right.length) {
+            if (left[i] > right[j]) {
+
+                if (rf) {
+                    res[k] = left[i];
+                    i++;
+                } else {
+                    res[k] = right[j];
+                    if (j == right.length - 1) {
+                        rf = true;
+                    } else {
+                        j++;
+                    }
+                }
+
+
+            } else {
+                if (lf) {
+                    res[k] = right[j];
+                    j++;
+                } else {
+                    res[k] = left[i];
+                    if (i == left.length - 1) {
+                        lf = true;
+                    } else {
+                        i++;
+                    }
+                }
+            }
+            k++;
+        }
+
+        return res;
+    }
+
+
+    /**
      * 给定一个非负整数 num。对于 0 ≤ i ≤ num 范围中的每个数字 i ，计算其二进制数中的 1 的数目并将它们作为数组返回。
+     * 动态规划
+     *
      * @param num
      * @return
      */
     public int[] countBits(int num) {
-        int a = num&(num-1);
-        System.out.println(a);
-        return null;
+        int[] res = new int[num + 1];
+
+        int b = 1, i = 0;
+
+        res[0] = 0;
+
+        while (b <= num) {
+
+            while (i < b && i + b <= num) {
+                res[i + b] = res[i] + 1;
+                i++;
+            }
+
+            i = 0;
+
+            b = b << 1;
+        }
+
+        return res;
     }
 
+    /**
+     * 191. 位1的个数
+     * 编写一个函数，输入是一个无符号整数，返回其二进制表达式中数字位数为 ‘1’ 的个数（也被称为汉明重量）
+     *
+     * @param n
+     * @return
+     */
+    public int hammingWeight(int n) {
+        int[] res = new int[n + 1];
+
+        int b = 1, i = 0;
+
+        res[0] = 0;
+
+        while (b <= n) {
+
+            while (i < b && i + b <= n) {
+                res[i + b] = res[i] + 1;
+                i++;
+            }
+
+            i = 0;
+
+            b = b << 1;
+        }
+
+        return res[n];
+    }
+
+    /**
+     * 1025. 除数博弈
+     * <p>
+     * 爱丽丝和鲍勃一起玩游戏，他们轮流行动。爱丽丝先手开局。
+     * <p>
+     * 最初，黑板上有一个数字 N 。在每个玩家的回合，玩家需要执行以下操作：
+     * <p>
+     * 选出任一 x，满足 0 < x < N 且 N % x == 0 。
+     * 用 N - x 替换黑板上的数字 N 。
+     * 如果玩家无法执行这些操作，就会输掉游戏。
+     * <p>
+     * 只有在爱丽丝在游戏中取得胜利时才返回 True，否则返回 false。假设两个玩家都以最佳状态参与游戏
+     *
+     * @param N
+     * @return
+     */
     public boolean divisorGame(int N) {
 
         boolean flag = false;
 
-        if(N == 1){
+        if (N == 1) {
             return true;
         }
 
-        for (int i = N-1; i > 0; i--) {
-            if(i == 1 && N-i==1){flag = !flag;break;}
-
-            if(N%i==0){
-                N=N-i;
+        for (int i = N - 1; i > 0; i--) {
+            if (i == 1 && N - i == 1) {
                 flag = !flag;
-                i=N-1;
+                break;
+            }
+
+            if (N % i == 0) {
+                N = N - i;
+                flag = !flag;
+                i = N - 1;
             }
 
         }
 
         return flag;
     }
+
+    /**
+     * 08.11. 硬币
+     * 硬币。给定数量不限的硬币，币值为25分、10分、5分和1分，编写代码计算n分有几种表示法。(结果可能会很大，你需要将结果模上1000000007)
+     * 1.DP
+     * 2.等差数列算法
+     *
+     * @param n
+     * @return
+     */
+    public int waysToChange(int n) {
+
+        /*int[] coins = {25,10,5,1};
+
+        for (int i = 0; i < coins.length; i++) {
+
+        }*/
+
+        int res = 0;
+
+        for (int i = 0; i * 25 <= n; i++) {
+            int c = n - i * 25;
+            int a = c / 10;
+            int b = c % 10 / 5;
+
+            res = (int) (res + ((long) (a + 1) * (a + b + 1)) % 1000000007) % 1000000007;
+
+            System.out.println(i + "/" + res);
+        }
+
+        return res;
+    }
+
+
 }
