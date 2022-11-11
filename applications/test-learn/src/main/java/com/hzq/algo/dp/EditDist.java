8a6df02f26bcf9b17a3fdde1172377736a721211
Huangzq <zhenqiang.huang@qizhidao.com>
2022-11-11 16:58:40 +0800

    编辑距离

--- applications/test-learn/src/main/java/com/hzq/algo/dp/EditDist.java
+++ applications/test-learn/src/main/java/com/hzq/algo/dp/EditDist.java
@@ -2,15 +2,39 @@ package com.hzq.algo.dp;
 
 /**
  * 编辑距离
- *
+ *   opt：删除 d-花费1 插入 i-花费1 替换 r-花费1
+ *   source：ab    target：acd
+ *   **************************
+ *   *     *   -  *  a  *  b  *
+ *   **************************
+ *   *  -  *   0  *  1  *  2  *
+ *   **************************
+ *   *     *      * d:2 * d:3 *
+ *   *  a  *   1  * i:2 * i:1 *
+ *   *     *      * u:0 * u:2 *
+ *   *     *      * r:0 * r:1 *
+ *   **************************
+ *   *     *      * d:1 * d:2 *
+ *   *  c  *   2  * i:3 * i:2 *
+ *   *     *      * u:2 * u:1 *
+ *   *     *      * r:1 * r:1 *
+ *   **************************
+ *   *     *      * d:2 * d:2 *
+ *   *  d  *   3  * i:4 * i:3 *
+ *   *     *      * u:3 * u:2 *
+ *   *     *      * r:2 * r:2 *
+ *   **************************
+ *   [0, 1]
+ *   [1, 1]
+ *   [2, 2]
  * @author Huangzq
  * @description
  * @date 2022/9/2 17:52
  */
 public class EditDist {
     public static void main(String[] args) {
-        String s1 = "abcdef";
-        String s2 = "acaaaf";
+        String s1 = "acd";
+        String s2 = "ab";
 
         char[] c1 = s1.toCharArray();
         char[] c2 = s2.toCharArray();
