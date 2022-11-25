2a996e5aca06d508b803163860d71c34620a58d0
sultan <hzqsultan@qq.com>
2022-11-25 17:50:13 +0800

    游艇租赁

---
+++ applications/test-learn/src/main/java/com/hzq/algo/dp/YachtRent.java
@@ -0,0 +1,20 @@
+package com.hzq.algo.dp;
+
+/**
+ * 游艇租赁
+ * @author Huangzq
+ * @description
+ * @date 2022/11/11 17:46
+ */
+public class YachtRent {
+    public static void main(String[] args) {
+        int rent[][] = {
+                {0,2,6,9,15,20},
+                {0,0,3,5,11,18},
+                {0,0,0,3,6,12},
+                {0,0,0,0,5,8},
+                {0,0,0,0,0,6},
+                {0,0,0,0,0,0}
+        };
+    }
+}
